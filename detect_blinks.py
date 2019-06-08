cbaff12ad3af2b82ac99e514aef33d72293e00fc
Jay Kang <jehkang@gmail.com>
2019-06-07 20:45:12 -0400

    Create detect_blinks.py

---
+++ detect_blinks.py
@@ -0,0 +1,125 @@
+# USAGE
+# python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat --video blink_detection_demo.mp4
+# python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat
+
+# import the necessary packages
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+import numpy as np
+import imutils
+import time
+import dlib
+import cv2
+import urllib
+
+def url_to_image(url):
+	# download the image, convert it to a NumPy array, and then read
+	# it into OpenCV format
+	resp = urllib.request.urlopen(url)
+	image = np.asarray(bytearray(resp.read()), dtype="uint8")
+	image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+	# return the image
+	return image
+
+def eye_aspect_ratio(eye):
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
+
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+
+	# return the eye aspect ratio
+	return ear
+
+def url_to_image(url):
+    resp = urllib.request.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
+ 
+# define two constants, one for the eye aspect ratio to indicate
+# blink and then a second constant for the number of consecutive
+# frames the eye must be below the threshold
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 3
+
+# initialize the frame counters and the total number of blinks
+COUNTER = 0
+TOTAL = 0
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(r"C:\Users\Will\camera\shape_predictor_68_face_landmarks.dat")
+
+# grab the indexes of the facial landmarks for the left and
+# right eye, respectively
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+# start the video stream thread
+print("[INFO] starting video stream thread...")
+
+# grab the frame from the threaded video file stream, resize
+# it, and convert it to grayscale
+# channels)
+frame = url_to_image(r"http://192.168.0.5/picture/1/current/")#192.168.86.87
+frame = imutils.resize(frame, width=450)
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+# detect faces in the grayscale frame
+rects = detector(gray, 0)
+
+# loop over the face detections
+for rect in rects:
+	# determine the facial landmarks for the face region, then
+	# convert the facial landmark (x, y)-coordinates to a NumPy
+	# array
+	shape = predictor(gray, rect)
+	shape = face_utils.shape_to_np(shape)
+
+	# extract the left and right eye coordinates, then use the
+	# coordinates to compute the eye aspect ratio for both eyes
+	leftEye = shape[lStart:lEnd]
+	rightEye = shape[rStart:rEnd]
+	leftEAR = eye_aspect_ratio(leftEye)
+	rightEAR = eye_aspect_ratio(rightEye)
+
+	# average the eye aspect ratio together for both eyes
+	ear = (leftEAR + rightEAR) / 2.0
+
+	# compute the convex hull for the left and right eye, then
+	# visualize each of the eyes
+	leftEyeHull = cv2.convexHull(leftEye)
+	rightEyeHull = cv2.convexHull(rightEye)
+	cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+	cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+	# check to see if the eye aspect ratio is below the blink
+	# threshold, and if so, increment the blink frame counter
+	if ear < EYE_AR_THRESH:
+		print("eyes closed")
+
+	# otherwise, the eye aspect ratio is not below the blink
+	# threshold
+	else:
+		# if the eyes were closed for a sufficient number of
+		# then increment the total number of blinks
+		if COUNTER >= EYE_AR_CONSEC_FRAMES:
+			TOTAL += 1
+
+		# reset the eye frame counter
+		COUNTER = 0
+
+
+# show the frame
+cv2.imshow("Frame", frame)
+cv2.waitKey(0)
